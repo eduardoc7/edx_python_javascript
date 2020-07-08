f081ff836ebe6e6351f6aaa757122f709219df7c
Eduardo Cordeiro <eduardo.cordeiro1@outlook.com>
2020-07-08 19:42:03 -0300

    finish to do list app

--- learning-django/primeiro-projeto/tasks/urls.py
+++ learning-django/primeiro-projeto/tasks/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+# um identificador único para as rotas de cada app
+# isso é feito para não gerar um conflito de namespace: duas rotas com o mesmo nome em diferentes aplicações
+app_name = 'tasks'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('add', views.add, name='add')
 ]
--- learning-django/primeiro-projeto/tasks/views.py
+++ learning-django/primeiro-projeto/tasks/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django import forms
 
-tasks = ['foo', 'bar', 'vaz']
+
+class NewTask(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 
 # Create your views here.
 def index(request):
+    # armazenando as listas em cada sessão do navegador
+    if 'tasks' not in request.session:
+        request.session['tasks'] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session['tasks']
+    })
+
+
+def add(request):
+    # fazer uma verificação no server-side para confirmar se o form está válido
+    if request.method == "POST":
+        form = NewTask(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session['tasks'] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTask()
     })
