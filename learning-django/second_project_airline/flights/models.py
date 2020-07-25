635a35991c96feee1d3387eea3be4a3ff62e2ddb
Eduardo Cordeiro <eduardo.cordeiro1@outlook.com>
2020-07-24 21:57:44 -0300

    created index page and show the first model

--- learning-django/second_project_airline/flights/models.py
+++ learning-django/second_project_airline/flights/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+# cria uma classe que herda de outra classe model do python
+
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.city} ({self.code})'
+
+
+class Flight(models.Model):
+    # dentro de um modelo é necessário passar todos os parametros e propriedades que este modelo terá.
+    # origin referenciando outra tabela (modelo) com o argumento on_delete.
+    # isto é, uma restrição para quando a referência for deletada,
+    # caso a tabela Airport, esse Flight também seja deletado.
+    # related_name - criará dentro de Airport um atributo 'departures'.
+    # sendo assim, agora a classe Airpot consegue saber todos as origens feitas por ela por cada Airport especifico.
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    duration = models.IntegerField()
+
+    def __str__(self):
+        # função que faz uma representação de caracteres do objeto do model
+        return f'{self.id}: {self.origin} to {self.destination}'
