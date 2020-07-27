4705d1d1acb211bf5c437f493bd15f11cbdc0b45
Eduardo Cordeiro <eduardo.cordeiro1@outlook.com>
2020-07-27 16:10:31 -0300

    created a page for show the flights details

--- learning-django/second_project_airline/flights/views.py
+++ learning-django/second_project_airline/flights/views.py
@@ -6,3 +6,10 @@ from .models import Flight
 
 def index(request):
     return render(request, "flights/index.html", {"flights": Flight.objects.all()})
+
+
+def flight(request, flight_id):
+    flight_details = Flight.objects.get(pk=flight_id) # aqui podendo também ser o flight_id do objeto | PK = primary key
+    return render(request, "flights/flight.html", {
+        "flight": flight_details
+    })
